12459ccf1789dce11009aefa4b2c8f79cf50e2a9
Sudarshan Wagh <sudarshanwaghvs@gmail.com>
2023-09-29 11:08:08 +0530

    End of section 17

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 import { store } from "../stores/store";
 import { router } from "../router/Routes";
 import { User, UserFormValues } from "../models/user";
+import { Photo, Profile } from "../models/profile";
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => {
@@ -83,9 +84,24 @@ const Account = {
   login: (user: UserFormValues) => request.post<User>('/account/login', user),
   register: (user: UserFormValues) => request.post<User>('/account/register', user)
 }
+
+const Profiles = {
+  get: (username: string) => request.get<Profile>(`/profiles/${username}`),
+  uploadPhoto: (file: Blob) => {
+    let formData = new FormData();
+    formData.append('File', file);
+    return axios.post<Photo>('photos', formData, {
+      headers: {'Content-Type': 'multipart/form-data'}
+    })
+  },
+  setMainPhoto: (id: string) => request.post(`/photos/${id}/setMain`, {}),
+  deletePhoto: (id: string) => request.delete(`/photos/${id}`)
+}
+
 const agent = {
   Activities,
-  Account
+  Account,
+  Profiles
 };
 
 export default agent;
